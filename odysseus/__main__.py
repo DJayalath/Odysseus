08e62b503d7be0b447ddc2bc41bce6575812f277
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-21 23:10:07 +0100

    Use Click to provide basic CLI

--- odysseus/__main__.py
+++ odysseus/__main__.py
@@ -1,6 +1,16 @@
+import click
+from .network import Network
 
-def main():
-    print("In main")
+@click.command()
+@click.argument('file')
+def main(file):
+    click.echo("\n*** Initialising network ***\n")
+    network = Network()
+
+    click.echo("\n*** Making predictions ***\n")
+    network.single_predict(file)
+
+    click.echo("\n*** Finished ***\n")
 
 if __name__ == '__main__':
     main()
